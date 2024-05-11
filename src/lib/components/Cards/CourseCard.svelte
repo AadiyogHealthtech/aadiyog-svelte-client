27f68063157d35b575f85c596922d403e1ae8373
Anurag Pal <apal895@gmail.com>
2024-05-11 13:04:44 +0530

    design fixes

--- src/lib/components/Cards/CourseCard.svelte
+++ src/lib/components/Cards/CourseCard.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	import type { HTMLAttributes } from 'svelte/elements';
 	import Dot from '$lib/icons/Dot.svelte';
 	import Star from '$lib/icons/Star.svelte';
 	import Bookmark from '$lib/icons/Bookmark.svelte';
-	import Course from '$lib/Images/Course.jpg';
 
-	interface $$Props extends HTMLAttributes<HTMLDivElement> {
-		id: string;
-		title: string;
-		topic: string;
-		duration: string;
-		videos: string;
-		rating: string;
-		reviews: string;
-		src?: string;
-		bookmarked?: boolean;
-	}
+	export let id: string;
+	export let title: string;
+	export let topic: string;
+	export let duration: string;
+	export let videos: string;
+	export let rating: string;
+	export let reviews: string;
+	export let src: string = '';
+	export let bookmarked: boolean = false;
 
 	const BASE_STYLES = 'w-52 px-4 py-3';
 
@@ -27,40 +23,41 @@
 	}
 </script>
 
-<div {...$$restProps} data-testid={$$restProps.id} class={BASE_STYLES}>
+<div data-testid={id} class={BASE_STYLES}>
 	<div class="relative">
-		<img class="w-52 h-52" alt="CourseImage" src={Course} />
+		<img class="w-52 h-52" alt="CourseImage" {src} />
 
 		<button class="absolute right-1 top-1" on:click={handleClick}>
 			<Bookmark
-				stroke={$$restProps.bookmarked ? 'black' : 'white'}
-				fill={$$restProps.bookmarked ? 'white' : '#868686'}
-				stroke_width={$$restProps.bookmarked ? '0.2' : '1.75'}
+				stroke={bookmarked ? 'black' : 'white'}
+				fill={bookmarked ? 'white' : '#868686'}
+				stroke_width={bookmarked ? '0.2' : '1.75'}
 			/>
 		</button>
 	</div>
-
-	<p class="flex font-semibold text-neutral-grey-2 text-base leading-6 mt-2 mb-2 text-left">
-		{$$restProps.title}
-	</p>
-
-	<p class="flex font-normal text-neutral-grey-3 text-sm leading-4 mb-2 text-left">
-		{$$restProps.topic}
-	</p>
-
-	<div class="flex flex-row items-center mb-2">
-		<p class="font-normal text-neutral-grey-3 text-sm leading-4 mr-2">{$$restProps.duration}</p>
-		<Dot />
-		<p class="font-normal text-neutral-grey-3 text-sm leading-4 ml-2">
-			{`${$$restProps.videos} videos`}
+	<div class="mt-2">
+		<p class="flex font-semibold text-neutral-grey-2 text-base leading-6 text-left">
+			{title}
 		</p>
-	</div>
 
-	<div class="flex flex-row items-center mb-2">
-		<p class="font-normal text-neutral-grey-3 text-sm leading-4 mr-2">{$$restProps.rating}</p>
-		<Star color="#F37003" width={12} height={12} />
-		<p class="font-normal text-neutral-grey-3 text-sm leading-4 ml-2">
-			{`(${$$restProps.reviews})`}
+		<p class="flex font-normal text-neutral-grey-3 text-sm leading-4 mb-2 text-left">
+			{topic}
 		</p>
+
+		<div class="flex items-center mb-2">
+			<p class="font-normal text-neutral-grey-3 text-sm leading-4 mr-2">{duration}</p>
+			<Dot />
+			<p class="font-normal text-neutral-grey-3 text-sm leading-4 ml-2">
+				{`${videos} videos`}
+			</p>
+		</div>
+
+		<div class="flex items-center mb-2">
+			<p class="font-normal text-neutral-grey-3 text-sm leading-4 mr-2">{rating}</p>
+			<Star color="#F37003" width={12} height={12} />
+			<p class="font-normal text-neutral-grey-3 text-sm leading-4 ml-2">
+				{`(${reviews})`}
+			</p>
+		</div>
 	</div>
 </div>
--- src/routes/courseDetails/Components/Courses.svelte
+++ src/routes/courseDetails/Components/Courses.svelte
@@ -22,7 +22,8 @@
 			duration: '1 hr 10 min',
 			videos: '5',
 			rating: '4.8',
-			reviews: '16'
+			reviews: '16',
+			src: '/assets/images/yoga-pose.jpg'
 		},
 		{
 			id: 'two',
@@ -31,7 +32,8 @@
 			duration: '1 hr 10 min',
 			videos: '5',
 			rating: '4.8',
-			reviews: '16'
+			reviews: '16',
+			src: '/assets/images/yoga-pose.jpg'
 		},
 		{
 			id: 'three',
@@ -40,7 +42,8 @@
 			duration: '1 hr 10 min',
 			videos: '5',
 			rating: '4.8',
-			reviews: '16'
+			reviews: '16',
+			src: '/assets/images/yoga-pose.jpg'
 		}
 	];
 
@@ -52,8 +55,8 @@
 	}
 </script>
 
-<div class="h-screen px-8 pt-8 flex flex-col items-start">
-	<div class="w-screen px-8 flex flex-row items-center justify-center">
+<div class="h-screen px-8 pt-8 flex flex-col items-start w-full overflow-x-hidden">
+	<div class="w-full flex flex-row items-center justify-center">
 		<MainLogo width={32} height={32} />
 		<h1 class="ml-2">Aadiyog</h1>
 		<button class="ml-auto">
@@ -61,40 +64,41 @@
 		</button>
 	</div>
 
-	<h2 class="px-8 pt-8 text-neutral-grey-4">Good morning</h2>
+	<h2 class=" pt-8 text-neutral-grey-4">Good morning</h2>
 
-	<div class="w-screen px-8 py-4 flex flex-row items-center justify-between">
+	<div class="w-full py-4 flex flex-row items-center justify-between">
 		<h1 class="text-neutral-grey-2">Recommended for you</h1>
 		<h3 class="text-neutral-grey-2">See All</h3>
 	</div>
 
-	<div class="flex w-full max-w-full overflow-x-auto">
-		{#each courses as obj, i}
-			<button class="flex-1" on:click={() => handleClick(i)}>
+	<div class="flex w-full">
+		{#each courses as course, i}
+			<div class="flex-1 shrink-0" on:click={() => handleClick(i)}>
 				<CourseCard
-					id={obj.id}
-					title={obj.title}
-					topic={obj.topic}
-					duration={obj.duration}
-					videos={obj.videos}
-					rating={obj.rating}
-					reviews={obj.reviews}
+					id={course.id}
+					title={course.title}
+					topic={course.topic}
+					duration={course.duration}
+					videos={course.videos}
+					rating={course.rating}
+					reviews={course.reviews}
+					src={course.src}
 				/>
-			</button>
+			</div>
 		{/each}
 	</div>
 
-	<div class="w-screen px-8 py-4 flex flex-row items-center justify-between">
+	<div class="w-full py-4 flex flex-row items-center justify-between">
 		<h1 class="text-neutral-grey-2">Explore</h1>
 		<h3 class="text-neutral-grey-2">See All</h3>
 	</div>
 
-	<div class="w-screen px-8 py-4 flex flex-row items-center justify-between">
+	<div class="w-full py-4 flex flex-row items-center justify-between">
 		<h1 class="text-neutral-grey-2">Free courses</h1>
 		<h3 class="text-neutral-grey-2">See All</h3>
 	</div>
 
-	<div class="flex h-screen flex-col justify-end">
+	<div class="absolute bottom-0 left-0 w-full">
 		<BottomTabBar {tabs} id="One" />
 	</div>
 </div>
